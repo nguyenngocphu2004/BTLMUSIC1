3f94ed97dbe7cadc9e91466b5376d276b388e70f
nguyenngocphu2004 <141038601+nguyenngocphu2004@users.noreply.github.com>
2023-08-07 11:13:12 +0700

    demo
    123

---
+++ js/index.js
@@ -0,0 +1,248 @@
+let btnUser = document.getElementById("btn-user");
+let vUser = document.getElementById("v-user");
+let items = document.querySelectorAll(".slider .item");
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
+let active = 0;
+
+function loadArtists() {
+  fetch("assests/data/artists.json").then(res => res.json()).then(data => {
+      let artistsList = document.getElementById("artists");
+      let artistsHtml = "";
+      for (let artist of data) {
+        artistsHtml += `
+          <li>
+            <div>
+              <div>
+                <a href="">
+                  <img src="${artist.image}" alt="">
+                  <i class="fa-solid fa-play"></i>
+                </a>    
+              </div>
+              <a href="">${artist.name}</a>
+            </div>
+          </li>
+        `;
+      }
+      artistsList.innerHTML = artistsHtml;
+    })
+}
+
+function loadCategorys() {
+  fetch("assests/data/categorys.json").then(res => res.json()).then(data => {
+      let categorysList = document.getElementById("categorys");
+      let categorysHtml = "";
+      for (let category of data) {
+        categorysHtml += `
+          <li>
+            <div>
+              <div>
+                <a href="">
+                  <img src="${category.image}" alt="">
+                  <i class="fa-solid fa-play"></i>
+                </a>    
+              </div>
+              <a href="">${category.name}</a>
+            </div>
+          </li>
+        `;
+      }
+      categorysList.innerHTML = categorysHtml;
+    })
+}
+
+/* Click and show */
+function clickShow(e) {
+  e.style.display = "block";
+}
+function clickEnd(e) {
+  e.style.display = "none"
+}
+function clickFull(btnE, e) {
+  let userClicked = false;
+  let userClicking = false;
+  btnE.onclick = function () {
+    if (userClicked) {
+      e.style.display = "none";
+      userClicked = false;
+    } else {
+      e.style.display = "block";
+      userClicked = true;
+    }
+  };
+  // Xử lý sự kiện mouseup khi người dùng kết thúc click
+  document.addEventListener("mouseup", function () {
+    userClicking = false;
+  });
+  // Sự kiện mousedown khi người dùng bắt đầu click
+  btnUser.addEventListener("mousedown", function () {
+    userClicking = true;
+  });
+  document.addEventListener("click", function (event) {
+    if (event.target === vUser || event.target === btnUser)
+      return;
+    else {
+      vUser.style.display = "none";
+      userClicked = false;
+    }
+  });
+}
+/* Slider */
+function loadShow() {
+  let stt = 0;
+  items[active].style.transform = `none`;
+  items[active].style.zIndex = 1;
+  items[active].style.filter = "none";
+  items[active].style.opacity = 1;
+  for (var i = active + 1; i < items.length; i++) {
+    stt++;
+    items[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
+    items[i].style.zIndex = -stt;
+    items[i].style.filter = "blur(5px)";
+    items[i].style.opacity = stt > 2 ? 0 : 0.6;
+  }
+  stt = 0;
+  for (var i = active - 1; i >= 0; i--) {
+    stt++;
+    items[i].style.transform = `translateX(${-120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(1deg)`;
+    items[i].style.zIndex = -stt;
+    items[i].style.filter = "blur(5px)";
+    items[i].style.opacity = stt > 2 ? 0 : 0.6;
+  }
+}
+
+
+
+window.onload = function () {
+  loadArtists();
+  loadCategorys();
+  /* Click user */
+  clickFull(btnUser, vUser);
+  /* Slider animation */
+  loadShow();
+  next.onclick = function () {
+    active = active + 1 < items.length ? active + 1 : active;
+    loadShow();
+  };
+  prev.onclick = function () {
+    active = active - 1 >= 0 ? active - 1 : active;
+    loadShow();
+  };
+  //AutoSlide
+  let autoSlideInterval = 5000;
+  function autoSlideShow() {
+    active = (active + 1) % items.length;
+    loadShow();
+  }
+  let autoSlideTimer = setInterval(autoSlideShow, autoSlideInterval);
+  // // Tạm dừng chuyển slide tự động khi chuột di chuyển qua slider
+  // slider.addEventListener("mouseover", () => {
+  //   clearInterval(autoSlideTimer);
+  // });
+  
+  // // Tiếp tục chuyển slide tự động khi chuột rời khỏi slider
+  // slider.addEventListener("mouseout", () => {
+  //   autoSlideTimer = setInterval(autoSlideShow, autoSlideInterval);
+  // });
+  
+};
+let register=document.querySelector('.js-register')
+let modal=document.querySelector('.js-modal')
+let modal1=document.querySelector('.js-modal1')
+let login=document.querySelector('.js-login')
+let exit=document.querySelector('.exit')
+let exit1=document.querySelector('.exit1')
+function showLog(){
+  modal.classList.add('open')
+}
+register.addEventListener('click',function (event){
+  event.preventDefault();
+  showLog();
+});
+function hideLog(){
+  modal.classList.remove('open')
+}
+exit.addEventListener('click',function (event){
+  event.preventDefault();
+  hideLog();
+});
+function showLog1(){
+  modal1.classList.add('open')
+}
+function hideLog1(){
+  modal1.classList.remove('open')
+}
+login.addEventListener('click',function (event){
+  event.preventDefault();
+  showLog1();
+});
+exit1.addEventListener('click',function (event){
+  event.preventDefault();
+  hideLog1();
+});
+modal.addEventListener('click',function (event){
+  event.preventDefault();
+  hideLog();
+})
+modal1.addEventListener('click',function (event){
+  event.preventDefault();
+  hideLog1();
+})
+function validate(e){
+  if(e.value==="")
+      {
+        return true;
+      }
+  return false;
+}
+function signup(){
+      event.preventDefault();
+      var email=document.getElementById("signup-email")
+      var password=document.getElementById("signup-password")
+      var Repassword=document.getElementById("Resignup-password")
+      if(validate(email)===true||validate(password)===true)
+      {
+        alert("Chưa điền đầy đủ thông tin")
+        return;
+      }
+      if(password.value!==Repassword.value)
+      {
+        alert("Vui lòng nhập lại đúng mật khẩu")
+        return;
+      }
+      var user={
+        email:email.value,
+        password:password.value,
+      }
+      var json=JSON.stringify(user)
+      localStorage.setItem(email,json)
+      alert("Dang ky thanh cong!")
+      hideLog();
+      register.innerText="Chào"
+      login.innerText=email.value
+      return;  
+}
+function dangnhap(){
+  event.preventDefault();
+  var email=document.getElementById("login-email")
+  var password=document.getElementById("login-password")
+  if(validate(email)===true||validate(password)===true)
+  {
+    alert("Chưa điền đầy đủ thông tin")
+    return;
+  }
+  var user1=localStorage.getItem(email)
+  var data=JSON.parse(user1)
+  if(email.value===data.email&&password.value===data.password)
+  {
+    alert("Đăng nhập thành công")
+    hideLog1();
+    register.innerText="Chào"
+    login.innerText=email.value
+    return;
+  }
+  else{
+    alert("Email hoặc mật khâủ không đúng vui lòng nhập lại")
+    return;
+  }
+}
